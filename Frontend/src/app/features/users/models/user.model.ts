3249439be1d335e6ffe6087def7fb8fdc5126b9a
lemuayala <lemuayala@gmail.com>
2025-05-01 20:55:50 -0300

    interfaces para user

---
+++ Frontend/src/app/features/users/models/user.model.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  reputation: number;
+  vehicleDetails: string | null;
+}
+
+export interface CreateUserDto {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  vehicleDetails: string | null;
+}
+
+export interface UpdateUserDto extends CreateUserDto {
+  reputation?: number;
+}

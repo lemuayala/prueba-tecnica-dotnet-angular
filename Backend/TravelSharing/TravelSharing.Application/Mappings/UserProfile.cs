22143dc2e00a5d82024b197709c6fbc4c421cde2
lemuayala <lemuayala@gmail.com>
2025-05-01 00:47:38 -0300

    Se agreag automapper y logger

---
+++ Backend/TravelSharing/TravelSharing.Application/Mappings/UserProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using TravelSharing.Application.DTOs.User;
+using TravelSharing.Domain.Entities;
+
+namespace TravelSharing.Application.Mappings;
+
+public class UserProfile: Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDto>();
+        CreateMap<CreateUserDto, User>();
+        CreateMap<UpdateUserDto, User>();
+    }
+}

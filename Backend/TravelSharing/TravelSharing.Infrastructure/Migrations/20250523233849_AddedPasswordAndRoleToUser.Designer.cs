fdee59aa18226321a32550ff4f2342a382cb67f4
lemuayala <lemuayala@gmail.com>
2025-05-23 22:29:52 -0300

    Se sube migration AddedPasswordAndRoleToUser es añade pass y rol para el usuario

--- Backend/TravelSharing/TravelSharing.Infrastructure/Migrations/20250523233849_AddedPasswordAndRoleToUser.Designer.cs
+++ Backend/TravelSharing/TravelSharing.Infrastructure/Migrations/20250523233849_AddedPasswordAndRoleToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelSharing.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using TravelSharing.Infrastructure.Persistence;
 namespace TravelSharing.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250523233849_AddedPasswordAndRoleToUser")]
+    partial class AddedPasswordAndRoleToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

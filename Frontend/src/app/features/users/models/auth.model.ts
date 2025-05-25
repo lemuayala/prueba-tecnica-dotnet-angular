96ec4956d1cefa845f5996f76211bf1a72f1e4e5
lemuayala <lemuayala@gmail.com>
2025-05-24 22:24:38 -0300

    Se agregan interfaces para login

--- Frontend/src/app/features/users/models/auth.model.ts
+++ Frontend/src/app/features/users/models/auth.model.ts
@@ -5,3 +5,17 @@ export interface RegisterUserDto {
   password: string;
   vehicleDetails?: string | null;
 }
+
+export interface LoginUserDto {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponseDto {
+  token: string;
+  expiration: string;
+  userId: string;
+  email: string;
+  name: string;
+  role: string;
+}

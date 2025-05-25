8491163d7e2dcf2ae1c675d3677f460317d14bc5
lemuayala <lemuayala@gmail.com>
2025-05-24 22:29:54 -0300

    Agregamos RehydrateAuthState para manejar expiracion del token de sesion y mantener la sesion abierta

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { UserActions } from './features/users/store/user.actions';
+import { LoginResponseDto } from './features/users/models/auth.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'travel-sharing-frontend';
+  private store = inject(Store);
+
+  ngOnInit(): void {
+    this.tryRehydrateAuthState();
+  }
+
+  private tryRehydrateAuthState(): void {
+    const token = localStorage.getItem('authToken');
+    const expirationString = localStorage.getItem('tokenExpiration');
+    const currentUserString = localStorage.getItem('currentUser');
+
+    if (token && expirationString && currentUserString) {
+      const expirationDate = new Date(expirationString);
+      if (expirationDate > new Date()) {
+        // Verificar si el token no ha expirado
+        try {
+          const currentUserData = JSON.parse(currentUserString);
+          const rehydratedResponse: LoginResponseDto = {
+            token: token,
+            expiration: expirationString,
+            ...currentUserData,
+          };
+          this.store.dispatch(
+            UserActions.rehydrateAuthState({ response: rehydratedResponse })
+          );
+        } catch (e) {
+          console.error('Error parsing currentUser from localStorage', e);
+        }
+      } else {
+        // Token expirado, limpiar localStorage
+        localStorage.removeItem('authToken');
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('tokenExpiration');
+      }
+    }
+  }
 }
